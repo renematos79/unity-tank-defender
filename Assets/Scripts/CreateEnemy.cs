38c33cffa8fe794b91bad27554c62781664f52c8
renato matos <renato.matos79@gmail.com>
2017-06-26 03:48:35 -0300

    inimigos

---
+++ Assets/Scripts/CreateEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour {
+
+	public GameObject Enemy;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateEnemyShip", 5.0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateEnemyShip(){
+		float y = 10.0f * Random.value - 5;
+		var enemy = Instantiate (Enemy);
+		enemy.transform.position = new Vector2 (10.0f, y);
+	}
+}
---
+++ Assets/Scripts/HandleWaves.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleWaves : MonoBehaviour {
+
+	public Renderer Quad;
+	public float OffsetSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		this.Quad = GetComponent<Renderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var offset = new Vector2(Time.deltaTime * this.OffsetSpeed, 0);
+		this.Quad.material.mainTextureOffset += offset;
+	}
+}
